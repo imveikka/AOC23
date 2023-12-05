19c32970e577d06cb038f6be3b045bc1729f1811
imveikka <imveikka@gmail.com>
2023-12-06 00:48:37 +0200

    Day 5: solved !!!!!!

--- 00101/silver.py
+++ 00101/silver.py
@@ -1,6 +1,16 @@
+"""
+--- Day 5: If You Give A Seed A Fertilizer ---
+
+Took longer to understand what to do.
+
+@imveikka
+"""
+
+
 from sys import argv
 import numpy as np
 from itertools import pairwise
+from tqdm import tqdm
 
 with open(argv[1], 'r') as f:
     lines = f.read().split('\n\n')
@@ -13,14 +23,19 @@ for line in lines[1:]:
     from_to, _ = data[0].split()
     mappings[from_to] = []
     for ranges in data[1:]:
-        start_dest, start_source, inc = np.fromstring(ranges, dtype=int, sep=' ')
-        mappings[from_to] += (range(start_source, start_source + inc), 
-                              range(start_dest, start_dest + inc))
+        s_d, s_s, inc = np.fromstring(ranges, dtype=int, sep=' ')
+        mappings[from_to] += [(range(s_s, s_s + inc),
+                               range(s_d, s_d + inc))]
 
+silver = np.inf
 for seed in seeds:
-    for source, dest in pairwise(mappings)):
+    foo = int(seed)
+    for pairs in mappings.values():
+        for source, dest in pairs:
+            if foo in source:
+                foo = dest.start + (foo - source.start)
+                break
+    silver = min(silver, foo)
 
+print(silver)
 
-    
-
-    
